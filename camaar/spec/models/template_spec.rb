704c26291dfe0e486c42cbc5bfa0c2ffaf9acf91
luisgds <luisgdscat10@gmail.com>
2024-06-10 11:22:50 -0300

    #30 rspec da model template

--- camaar/spec/models/template_spec.rb
+++ camaar/spec/models/template_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
-
+=begin
+Recomendado colocar na model template pra ficar mais consistente
+validates :nome , presence: true
+=end
 RSpec.describe Template, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it " é valido" do
+    template = build(:template)
+    expect(template).to be_valid
+  end
+  it "não é valido, pois nao" do
+    template = build(:template, nome: nil)
+    expect(template).to_not be_valid
+  end
 end
