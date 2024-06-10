aafca2d32f682eb80715a3b427ad76eb50a73639
luisgds <luisgdscat10@gmail.com>
2024-06-10 16:52:14 -0300

    #23 - Validates e rspec

--- camaar/app/models/user.rb
+++ camaar/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :matriculas
   has_many :turmas, through: :matriculas
+  validates :nome ,presence: true
+  validates :email ,presence: true
+  validates :curso ,presence: true
+  validates :matricula ,presence: true
+  validates :formacao ,presence: true
+  validates :ocupacao ,presence: true
 end
--- camaar/spec/models/user_spec.rb
+++ camaar/spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "é uma user valida" do
+    user = build(:user)
+    expect(user).to  be_valid
+  end
+  it "nao é uma user valida, pois nao tem email" do
+    user = build(:user, email:nil)
+    expect(user).to_not  be_valid
+  end
+  it "nao é uma user valida, pois nao tem matricula" do
+    user = build(:user, matricula:nil)
+    expect(user).to_not  be_valid
+  end
+  it "nao é uma user valida, pois nao tem curso" do
+    user = build(:user, curso:nil)
+    expect(user).to_not  be_valid
+  end
+  it "nao é uma user valida, pois nao tem nome" do
+    user = build(:user, nome:nil)
+    expect(user).to_not  be_valid
+  end
+  it "nao é uma user valida, pois nao tem formação" do
+    user = build(:user, formacao:nil)
+    expect(user).to_not  be_valid
+  end
+  it "nao é uma user valida, pois nao tem ocupação" do
+    user = build(:user, ocupacao:nil)
+    expect(user).to_not  be_valid
+  end
 end
