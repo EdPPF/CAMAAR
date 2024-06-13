1620f248583ed8667ee042b2e1e11ced9157c437
luisgds <luisgdscat10@gmail.com>
2024-06-13 12:02:33 -0300

    #43 -Controller Template

--- camaar/app/controllers/templates_controller.rb
+++ camaar/app/controllers/templates_controller.rb
@@ -1,2 +1,24 @@
 class TemplatesController < ApplicationController
+  def show
+    id = params[:id]
+    @template = Template.find(id)
+  end
+  def index
+    @template = Template.all
+  end
+  def new
+    @template = Template.new
+  end
+  def create
+    @template = Template.new(template_params)
+    if @template.save
+      redirect_to templates_path, notice: "#{@template.nome} created."
+    else
+      flash[:alert] = "template could not be created: " + @template.errors.full_messages.join(", ")
+      render 'new'
+    end
+  end
+  def edit
+    @template = Template.find params[:id]
+  end
 end
