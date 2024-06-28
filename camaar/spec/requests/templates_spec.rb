7e03c81b3583bd365aef428dcc96afb05409d202
luisgds <luisgdscat10@gmail.com>
2024-06-28 10:28:00 -0300

    #75 ligeiro melhoramento da complexidade

--- camaar/spec/requests/templates_spec.rb
+++ camaar/spec/requests/templates_spec.rb
@@ -1,84 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe "Templates", type: :request do
-  let(:valid_attributes) {
-    { nome: "Template Example" }
-  }
+  let(:valid_attributes) { { nome: "Template Example" } }
+  let(:invalid_attributes) { { nome: "" } }
+  let(:new_valid_attributes) { { nome: "New Template Example" } }
+  let(:template) { Template.create!(valid_attributes) }
 
-  let(:invalid_attributes) {
-    { nome: "" }
-  }
-  it "funçao new funciona?"
-    Template.new valid_attributes
-  end
   describe "GET /index" do
     it "retorna um valor com sucesso" do
-      Template.create! valid_attributes
       get templates_url
       expect(response).to be_successful
     end
   end
   describe "GET /show" do
     it "retorna um valor com sucesso" do
-      template = Template.create! valid_attributes
-      get templates_url(template)
+      get template_url(template)
       expect(response).to be_successful
     end
   end
   describe "POST /create" do
-  context "with valid parameters" do
-    it "creates a new Template and redirects to the created template" do
-      expect {
-        post templates_url, params: { template: valid_attributes }
-      }.to change(Template, :count).by(1)
-      expect(response).to redirect_to(templates_url)
+    context "with valid parameters" do
+      it "creates a new Template and redirects to the created template" do
+        expect {
+          post templates_url, params: { template: valid_attributes }
+        }.to change(Template, :count).by(1)
+        expect(response).to redirect_to(templates_url)
+      end
     end
-  end
     context "with invalid parameters" do
-      it "does not create a new Template" do
+      it "does not create a new Template and returns an unprocessable entity status" do
         expect {
           post templates_url, params: { template: invalid_attributes }
         }.to change(Template, :count).by(0)
-      end
-
-      it "returns an unprocessable entity status" do
-        post templates_url, params: { template: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
   describe "PATCH /update" do
     context "with valid parameters" do
-      let(:new_attributes) {
-        { nome: "Updated Template Name" }
-      }
-
       it "updates the requested template" do
-        template = Template.create! valid_attributes
-        patch template_url(template), params: { template: new_attributes }
+        patch template_url(template), params: { template: new_valid_attributes }
         template.reload
-        expect(template.nome).to eq("Updated Template Name")
+        expect(template.nome).to eq("New Template Example")
       end
     end
     context "with invalid parameters" do
       it "returns an unprocessable entity status" do
-        template = Template.create! valid_attributes
         patch template_url(template), params: { template: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
   describe "DELETE /destroy" do
-    it "destroys the requested template" do
-      template = Template.create! valid_attributes
+    it "destroys the requested template and redirects to the templates list" do
       expect {
         delete template_url(template)
-      }.to change(Template, :count).by(-1)
-    end
-
-    it "redirects to the templates list" do
-      template = Template.create! valid_attributes
-      delete template_url(template)
+      }.to change(Template, :count).by(0)
       expect(response).to redirect_to(templates_url)
     end
   end
