6a8d6f0e9299bc6659dd2aae66dc630763c26c2f
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-06-28 16:35:36 -0300

    [DOCS] materias_controller.rb

--- camaar/app/controllers/materias_controller.rb
+++ camaar/app/controllers/materias_controller.rb
@@ -1,4 +1,14 @@
+##
+# Responsável por gerenciar as matérias cadastradas no sistema.
 class MateriasController < ApplicationController
+
+    ##
+    # Lista todas as matérias cadastradas no sistema.
+    #
+    # Retorno:: depende do formato da requisição:
+    # - HTML: renderiza a view associada.
+    # - JSON: renderiza um JSON com todas as matérias.
+
     def index
         @materias = Materia.all
 
@@ -7,6 +17,17 @@ class MateriasController < ApplicationController
             format.json { render json: @materias }
         end
     end
+
+
+    ##
+    # Mostra uma matéria específica.
+    #
+    # Parâmetros:: o ID da matéria, passado pela rota.
+    #
+    # Retorno:: depende do formato da requisição:
+    # - HTML: renderiza a view associada.
+    # - JSON: renderiza um JSON com a matéria.
+
     def show
         @materia = Materia.find(params[:id])
 
@@ -16,13 +37,26 @@ class MateriasController < ApplicationController
         end
     end
 
+
+    ##
+    # Não utilizada:: cria uma instância de matéria, mas não salva no banco de dados.
+
     def new
         @materia = Materia.new
     end
 
+
+    ##
+    # Cria uma nova matéria.
+    #
+    # Parâmetros:: os atributos da matéria, na requisição.
+    #
+    # Retorno:: redireciona para a lista de matérias, com uma mensagem de sucesso.
+    # - Se os atributos da matéria forem inválidos, renderiza a view de criação, com uma mensagem de erro.
+
     def create
         @materia = Materia.new(materia_params)
-    
+
         if @materia.save
           redirect_to materias_path, notice: "#{@materia.nome} created."
         else
@@ -31,10 +65,24 @@ class MateriasController < ApplicationController
         end
     end
 
+
+    ##
+    # Não utilizada:: procura por uma matéria específica para editar.
+
     def edit
         @materia = Materia.find params[:id]
+        # TODO
     end
-    
+
+
+    ##
+    # Atualiza uma matéria específica.
+    #
+    # Parâmetros:: o ID da matéria e os atributos a serem atualizados, passados pela requisição.
+    #
+    # Retorno:: redireciona para a lista de matérias, com uma mensagem de sucesso.
+    # - Se a atualização falhar, renderiza a view de edição, com uma mensagem de erro.
+
     def update
         @materia = Materia.find(params[:id])
         if @materia.update(materia_params)
@@ -45,6 +93,13 @@ class MateriasController < ApplicationController
         end
     end
 
+
+    ##
+    # Deleta uma matéria específica.
+    #
+    # Parâmetros:: o ID da matéria, passado pela rota.
+    #
+    # Retorno:: redireciona para a lista de matérias, com uma mensagem de sucesso.
     def destroy
         @materia = Materia.find(params[:id])
         @materia.destroy
