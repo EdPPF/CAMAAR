b5750f47bc348f22c43d885da0c4df5c8e5cb97a
luisgds <luisgdscat10@gmail.com>
2024-06-28 09:44:30 -0300

    tirando duplicaçoes/limpando codigo

--- camaar/spec/requests/templates_spec.rb
+++ camaar/spec/requests/templates_spec.rb
@@ -26,18 +26,14 @@ RSpec.describe "Templates", type: :request do
     end
   end
   describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Template" do
-        expect {
-          post templates_url, params: { template: valid_attributes }
-        }.to change(Template, :count).by(1)
-      end
-
-      it "redirects to the created template" do
+  context "with valid parameters" do
+    it "creates a new Template and redirects to the created template" do
+      expect {
         post templates_url, params: { template: valid_attributes }
-        expect(response).to redirect_to(templates_url)
-      end
+      }.to change(Template, :count).by(1)
+      expect(response).to redirect_to(templates_url)
     end
+  end
     context "with invalid parameters" do
       it "does not create a new Template" do
         expect {
