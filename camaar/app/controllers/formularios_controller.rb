f4858dfe6f9615b6caedfdabf35c99f574339a9d
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-06-28 15:41:34 -0300

    [DOCS] formularios_controller.rb

--- camaar/app/controllers/formularios_controller.rb
+++ camaar/app/controllers/formularios_controller.rb
@@ -1,6 +1,17 @@
 require 'csv'
 
+## Responsável por controlar as ações CRUD de Formulários
+#
+
 class FormulariosController < ApplicationController
+
+    ##
+    # Lista todos os formulários.
+    #
+    # Retorno:: depende do formato da requisição:
+    # - HTML: renderiza a view associada.
+    # - JSON: renderiza um JSON com todos os formulários.
+
     def index
         @formularios = Formulario.all
         # mudei de render json: formularios, status: :ok para render json: formularios, status: :ok
@@ -10,6 +21,17 @@ class FormulariosController < ApplicationController
         end
     end
 
+
+    ##
+    # Mostra um formulário específico.
+    #
+    # Parâmetros:: o ID do formulário, passado pela rota.
+    #
+    # Retorno:: depende do formato da requisição:
+    # - HTML: renderiza a view associada.
+    # - JSON: renderiza um JSON com o formulário.
+    # - Se o formulário não for encontrado, renderiza um JSON com erro 404.
+
     def show
         @formulario = Formulario.find(params[:id])
          # render json: formulario, status: :ok
@@ -18,10 +40,18 @@ class FormulariosController < ApplicationController
             format.html
             format.json { render json: @formulario }
         end
-
     rescue StandardError => e
         render json: e, status: :not_found
     end
+
+
+    ##
+    # Cria um novo formulário.
+    #
+    # Parâmetros:: os atributos do formulário, na requisição.
+    #
+    # Retorno:: renderiza um JSON com o formulário criado.
+    # - Se os atributos do formulário forem inválidos, renderiza um JSON com erro 400.
 
     def create
         @formulario = Formulario.new(formulario_params)
@@ -30,6 +60,15 @@ class FormulariosController < ApplicationController
     rescue StandardError => e
         render json: e, status: :bad_request
     end
+
+
+    ##
+    # Atualiza um formulário específico.
+    #
+    # Parâmetros:: o ID do formulário e os atributos a serem atualizados, passados pela requisição.
+    #
+    # Retorno:: redireciona para a lista de formulários.
+    # - Se a atualização falhar, renderiza a view de edição e apresenta uma mensagem de erro com status 422.
 
     def update
         @formulario = Formulario.find(params[:id])
@@ -42,6 +81,14 @@ class FormulariosController < ApplicationController
         end
     end
 
+
+    ##
+    # Deleta um formulário específico.
+    #
+    # Parâmetros:: o ID do formulário, passado pela rota.
+    #
+    # Retorno:: se o formulário não for encontrado, renderiza um JSON com erro 404.
+
     def delete
         @formulario = Formulario.find(params[:id])
         formulario.destroy!
@@ -50,9 +97,20 @@ class FormulariosController < ApplicationController
         render json: e, status: :not_found
     end
 
+
+    ##
+    # Não utilizado:: esse método cria uma nova instância de Formulário, mas não a salva no banco de dados.
+
     def new
         @formularios = Formulario.new
     end
+
+
+    ##
+    # Gera uma arquivo CSV com todos os formulários.
+    #
+    # Retorno:: um arquivo CSV com todos os formulários.
+    # - Se não for possível gerar o arquivo, renderiza um JSON com erro 500.
 
     def export_csv
         @formularios = Formulario.all
@@ -60,7 +118,7 @@ class FormulariosController < ApplicationController
         respond_to do |format|
           format.csv { send_data generate_csv(@formularios), filename: "formularios-#{Date.today}.csv" }
         end
-      end
+    end
 
     private
 
