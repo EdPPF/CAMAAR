25f1ac80275788eaf6e390d55186ca00ae2a30b6
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-07-01 13:45:29 -0300

    [DOCS] respostas_controller.rb

--- camaar/app/controllers/respostas_controller.rb
+++ camaar/app/controllers/respostas_controller.rb
@@ -1,15 +1,39 @@
+##
+# Responsável por gerenciar as requisições referentes a Resposta.
+
 class RespostasController < ApplicationController
+
+  ##
+  # Lista todas as respostas cadastradas no sistema.
+  #
+  # Retorno:: renderiza um JSON com todas as respostas e status 200.
+
   def index
     respostas = Resposta.all
     render json: respostas, status: :ok
   end
 
+  ##
+  # Mostra uma resposta específica.
+  #
+  # Parâmetros:: o ID da resposta, passado pela rota.
+  #
+  # Retorno:: renderiza um JSON com a resposta e status 200.
+  # - Se a resposta não existir, renderiza um JSON com a mensagem de erro e status 404.
   def show
     resposta = Resposta.find(params[:id])
     render json: resposta, status: :ok
   rescue StandardError => e
     render json: e, status: :not_found
   end
+
+  ##
+  # Cria uma nova resposta.
+  #
+  # Parâmetros:: os atributos da resposta, na requisição.
+  #
+  # Retorno:: renderiza um JSON com a resposta criada e status 201.
+  # - Se os atributos da resposta forem inválidos, renderiza um JSON com a mensagem de erro e status 400.
 
   def create
     resposta = Resposta.new(resposta_params)
@@ -19,6 +43,14 @@ class RespostasController < ApplicationController
     render json: e, status: :bad_request
   end
 
+  ##
+  # Atualiza uma resposta existente.
+  #
+  # Parâmetros:: os atributos da resposta, na requisição.
+  #
+  # Retorno:: renderiza um JSON com a resposta atualizada e status 200.
+  # - Se a resposta não existir, renderiza um JSON com a mensagem de erro e status 404.
+
   def update
     resposta = Resposta.find(params[:id])
     resposta.update!(resposta_params)
@@ -27,6 +59,13 @@ class RespostasController < ApplicationController
     render json: e, status: :bad_request
   end
 
+  ##
+  # Deleta uma resposta existente.
+  #
+  # Parâmetros:: o ID da resposta, passado pela rota.
+  #
+  # Retorno:: renderiza um JSON com a mensagem de confirmação e status 200.
+  # - Se a resposta não existir, renderiza um JSON com a mensagem de erro e status 404.
   def delete
     resposta = Resposta.find(params[:id])
     resposta.destroy!
