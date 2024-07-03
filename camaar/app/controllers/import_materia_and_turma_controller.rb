09aa1ce6445929c541b86f5a8869e6002870c880
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-07-03 13:26:08 -0300

    [refactor] import_materia_and_turma_controller - Fix create

--- camaar/app/controllers/import_materia_and_turma_controller.rb
+++ camaar/app/controllers/import_materia_and_turma_controller.rb
@@ -1,25 +1,27 @@
 class ImportMateriaAndTurmaController < ApplicationController
   def create
-    materia_data = JSON.parse(params[:data], symbolize_names: true) rescue nil
-    if materia_data.present?
-      begin
-        import_materias(materia_data)
-        render json: { message: "Data imported successfully!" }, status: :created
-      rescue StandardError => e
-        render json: { message: "Error importing data: #{e.message}" }, status: :bad_request
-      end
-    else
+    materia_data = parse_json_data(params[:data])
+    import_materias(materia_data)
+    render json: { message: "Data imported successfully!" }, status: :created
+  rescue JSON::ParserError
       render json: { message: "Invalid JSON data format." }, status: :bad_request
-    end
+  rescue StandardError => e
+    render json: { message: "Error importing data: #{e.message}" }, status: :bad_request
   end
 
   private
+
+  def parse_json_data(data)
+    JSON.parse(data, symbolize_names: true)
+  # rescue JSON::ParserError
+  #   nil
+  end
 
   def import_materias(materia_data_array)
     materia_data_array.each do |materia_data|
       materia = Materia.create!(codigo: materia_data[:code], nome: materia_data[:name])
       turma_data = materia_data[:class]
-      turma = materia.turmas.create!(codigo: turma_data[:classCode], semestre: turma_data[:semester], horario: turma_data[:time])
+      materia.turmas.create!(codigo: turma_data[:classCode], semestre: turma_data[:semester], horario: turma_data[:time])
     end
   end
 end
