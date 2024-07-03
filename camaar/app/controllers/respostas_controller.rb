e3b576e6ccf5bfd09a4cb183d2b33587bda5354f
dendo1234 <37016869+dendo1234@users.noreply.github.com>
2024-07-03 20:42:24 +0000

    respostas de formularios

--- camaar/app/controllers/respostas_controller.rb
+++ camaar/app/controllers/respostas_controller.rb
@@ -19,17 +19,19 @@ class RespostasController < ApplicationController
     render json: e, status: :bad_request
   end
 
-  def bulk_create 
-    params.require(:template).require(:respostas).each do |key, resposta_param|
-      resposta_param = resposta_param.permit(:questao_id, :formulario_id, :texto)
-      Resposta.create!(resposta_param)
+  def bulk_create
+    if current_user
+      @formulario = Formulario.find(params.require(:template).require(:formulario_id))
+      params.require(:template).require(:respostas).each do |key, resposta_param|
+        resposta_param = resposta_param.permit(:questao_id, :formulario_id, :texto)
+        Resposta.create!(resposta_param)
+      end
+      current_user.formularios << @formulario
+      respond_to do |format|
+        format.html { redirect_to show_pending_formularios_path, notice: "Formulario respondido" }
+        format.json { render json: "Formualrio respondido", status: :created }
+      end
     end
-    respond_to do |format|
-      format.html { redirect_to show_pending_formularios_path, notice: "Formulario respondido" }
-      format.json { render json: "Formualrio respondido", status: :created }
-    end
-    rescue StandardError => e
-      render e, status: :bad_request
   end
 
   def update
