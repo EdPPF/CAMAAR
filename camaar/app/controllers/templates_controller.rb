e70e386c13dbe7380a2b1d992aa02ec32e3319f8
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-07-04 19:43:51 -0300

    [DOCS] templates_controller.rb

--- camaar/app/controllers/templates_controller.rb
+++ camaar/app/controllers/templates_controller.rb
@@ -1,17 +1,46 @@
+##
+# Responsável por controlar as ações de CRUD de templates.
+
 class TemplatesController < ApplicationController
-  #define o :set_template antes das funçoes abaixo
+  ##
+  # Define o template a ser utilizado.
+
   before_action :set_template, only: [:show, :edit, :update, :destroy]
+
+
+  ##
+  # Lista todos os templates.
+  #
+  # Retorno:: renderiza a variável de instância @templates na view.
 
   def index #chama todos os itens
     @templates = Template.all
   end
 
+
+  ##
+  # TODO: Mostra um template específico.
   def show # mostra o item de forma detalhada
+    # TODO
   end
+
+
+  ##
+  # Renderiza a view de criação de um novo template.
 
   def new # abre uma nova instancia
     @template = Template.new
   end
+
+
+  ##
+  # Cria um novo template.
+  #
+  # Parâmetros::
+  # template: Hash - os atributos do template a ser criado.
+  #
+  # Retorno:: redireciona para a página de templates e mostra uma mensagem de sucesso.
+  # - Se os atributos do template forem inválidos, renderiza a view de criação com uma mensagem de erro.
 
   def create # Processo de criação no banco de dados
     @template = Template.new(template_params)
@@ -23,8 +52,22 @@ class TemplatesController < ApplicationController
     end
   end
 
+
+  ##
+  # TODO: Renderiza a view de edição de um template.
   def edit # Ediçao da instancia em caso
   end
+
+
+  ##
+  # Atualiza um template específico.
+  #
+  # Parâmetros::
+  # id: int - o id do template a ser atualizado.
+  # template: Hash - os atributos do template a ser atualizado.
+  #
+  # Retorno:: redireciona para a página de templates e mostra uma mensagem de sucesso.
+  # - Se os atributos do template forem inválidos, renderiza a view de edição com uma mensagem de erro.
 
   def update # Atualização do template no banco de dados com a logistica
     if @template.update(template_params)
@@ -35,12 +78,21 @@ class TemplatesController < ApplicationController
     end
   end
 
+
+  ##
+  # Deleta um template específico.
+  #
+  # Parâmetros::
+  # id: int - o id do template a ser deletado.
+  #
+  # Retorno:: redireciona para a página de templates e mostra uma mensagem de sucesso.
+
   def destroy # Destroi o template em questao
     @template.destroy
     redirect_to templates_path, notice: "#{@template.nome} deleted."
   end
 
-  private # metodo privados que sao utilizados nas funçoes acima para escolher
+  private
 
   def set_template
     @template = Template.find(params[:id])
