7fa79ae5ebc653c1a0b5cdc366ac109e070999a9
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-07-04 19:31:39 -0300

    [DOCS] matriculas_controller.rb

--- camaar/app/controllers/matriculas_controller.rb
+++ camaar/app/controllers/matriculas_controller.rb
@@ -1,8 +1,27 @@
+##
+# Responsável por gerenciar as matrículas cadastradas no sistema.
+
 class MatriculasController < ApplicationController
+
+  ##
+  # Lista todas as matrículas.
+  #
+  # Retorno:: renderiza um JSON com todas as matrículas e status 200.
+
   def index
     matriculas = Matricula.all
     render json: matriculas, status: :ok
   end
+
+
+  ##
+  # Cria uma nova matrícula.
+  #
+  # Parâmetros::
+  # matricula: Hash - os atributos da matrícula a ser criada.
+  #
+  # Retorno:: renderiza um JSON com a matrícula criada e status 201.
+  # - Se os atributos da matrícula forem inválidos, renderiza um JSON com a mensagem de erro e status 400.
 
   def create
     matricula = Matricula.new(matricula_params)
@@ -11,6 +30,16 @@ class MatriculasController < ApplicationController
   rescue StandardError => e
     render json: e, status: :bad_request
   end
+
+
+  ##
+  # Deleta uma matrícula específica.
+  #
+  # Parâmetros::
+  # id: int - o id da matrícula a ser deletada.
+  #
+  # Retorno:: renderiza um JSON com a mensagem de sucesso e status 200.
+  # - Se a matrícula não existir, renderiza um JSON com a mensagem de erro e status 404.
 
   def delete
     matricula = Matricula.find(params[:id])
