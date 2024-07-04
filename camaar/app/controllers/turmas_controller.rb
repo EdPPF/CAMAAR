8b19a8a9d89f14425c09532ad81d0d9d9671fba8
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-07-04 19:45:59 -0300

    [DOCS] turmas_controller.rb

--- camaar/app/controllers/turmas_controller.rb
+++ camaar/app/controllers/turmas_controller.rb
@@ -1,8 +1,26 @@
+##
+# Responsável por gerenciar as requisições referentes a turmas.
+
 class TurmasController < ApplicationController
+
+  ##
+  # Lista todas as turmas cadastradas no sistema.
+  #
+  # Retorno:: renderiza um JSON com todas as turmas e status 200.
+
   def index
     turmas = Turma.all
     render json: turmas, status: :ok
   end
+
+
+  ##
+  # Mostra uma turma específica.
+  #
+  # Parâmetros::
+  # id: int - o id da turma a ser mostrada.
+  #
+  # Retorno:: renderiza um JSON com a turma e status 200.
 
   def show
     turma = Turma.find(params[:id])
